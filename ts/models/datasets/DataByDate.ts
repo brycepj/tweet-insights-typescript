53600e9d8674fc0c9354511a98124302a7702ebf
bjohnson <bjohnson@Opt-7010-2190BYI>
2014-04-30 09:59:21 -0400

    fix merge conflict

--- ts/models/datasets/DataByDate.ts
+++ ts/models/datasets/DataByDate.ts
@@ -2,16 +2,10 @@ module app {
 
     export module models {
 
-<<<<<<< HEAD
-        
-=======
->>>>>>> 8e268130f50828c1832e61bf5f6ba4bb85ab429c
         export class DataByDate {
 
-            dataSet:any;
-            model:any;
-<<<<<<< HEAD
-=======
+            dataSet: any;
+            model: any;
 
             constructor(freshData) {
 
@@ -22,35 +16,16 @@ module app {
                 console.log(this.model);
             }
 
-            init():any {
+            init(): any {
 
                 var freshData = this.dataSet;
 
                 this.model = app.processors.parseDataByDate(freshData);
 
             }
->>>>>>> 8e268130f50828c1832e61bf5f6ba4bb85ab429c
-
-            constructor(freshData) {
-
-                this.dataSet = freshData;
-                this.model = {};
-
-                this.init();
-                console.log(this.model);
-            }
-
-            init():any {
-
-                var freshData = this.dataSet;
-
-                this.model = app.processors.parseDataByDate(freshData);
-
 
 
         }
-
-}
     }
 
 
